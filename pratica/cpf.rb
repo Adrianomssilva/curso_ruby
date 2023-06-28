843fb7f6952f6b29f2dbb81771554c93464807c8
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-28 16:43:18 -0300

    cpf_validator frist commit

---
+++ pratica/cpf.rb
@@ -0,0 +1,15 @@
+require "cpf_validator"
+
+def check_cpf(num_cpf)
+   if CPF.valid(num_cpf)
+      puts "Esse cpf é válido"
+   else
+      puts "Esse cpf não é válido"
+   end
+
+end
+
+print "Digite seu cpf aqui: "
+num_cpf = gets.to_f
+
+check_cpf(num_cpf)
