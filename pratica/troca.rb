df3e5233c767ad946cd682fbce3c93519d2add88
devadrsilva <adriano.mssilva1@gmail.com>
2023-06-29 23:15:12 -0300

    teste troca

--- pratica/troca.rb
+++ pratica/troca.rb
@@ -4,4 +4,24 @@ arr = [3,2,1,0]
 # se sim arr0 troca com arr1
 # arrlength -1 é o limite
 # arri < arr i+1
-# 
+def bubble_sort(array)
+  array_length = array.size
+  return array if array_length <= 1
+
+  loop do
+    # we need to create a variable that will be checked so that we don't run into an infinite loop scenario.
+    swapped = false
+
+    # subtract one because Ruby arrays are zero-index based
+    (array_length-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
+  end
+
+  array
+end
