1ebbb4d9a1687ac81afbb04de7e74b3a63525523
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-03 16:01:25 -0300

    novas aulas

---
+++ POO/poli.rb
@@ -0,0 +1,36 @@
+#criar uma lista de objetos com a mesma função
+#escolhi como objeto coisas que escrevem: lápis, caneta e teclado
+
+class Objeto
+   def escrever
+      "Escrevendo"
+      
+   end
+   
+end
+
+class Lapis < Objeto
+   def escrever
+      "Escrevendo à lápis"
+   end
+end
+
+class Caneta < Objeto
+   def escrever
+      "Escrevendo à caneta"
+      
+   end
+   
+end
+
+class Teclado < Objeto
+
+end
+
+lapis = Lapis.new
+caneta = Caneta.new
+teclado = Teclado.new
+
+puts lapis.escrever
+puts caneta.escrever
+puts teclado.escrever
