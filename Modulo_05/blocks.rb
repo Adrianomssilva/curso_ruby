47432c0edabbecf749af03a484a45ba4fa8e626e
devadrsilva <adriano.mssilva1@gmail.com>
2023-07-10 22:10:36 -0300

    Blocks podem ser aplicados dentro de blocks

--- Modulo_05/blocks.rb
+++ Modulo_05/blocks.rb
@@ -7,3 +7,19 @@ end
 4.times {puts "teste"}
 
 #  Essas são as duas formas de escrever... Como posso aplicar isso em uma operação?
+#  no booblesort usamos block dentro de block
+
+loop do 
+
+   swapped = false
+
+   array.size.times do |i|   
+# |i| index começa do 0 
+      if array [i] > array[i+1]
+      array [i], array [i+1] = array [i+1], array[i]
+      swapped = true
+      end
+   end
+break if not swapped
+
+end
